67911d151ff02648a45b31ba3cd5ede35cb6e161
ryo106 <s86.shimauchi.ryosuke@gmail.com>
2021-01-01 22:34:47 +0900

    チャットルーム作成バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,22 +4,28 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
 
+  has_many :room_users
+  has_many :rooms, through: :room_users
+  has_many :messages
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  with_options presence: true do
-    validates :nickname
-    validates :birthday
 
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]/ } do
-      validates :first_name
-      validates :last_name
-    end
+  with_options on: :registrations_new do |new_record|
+    with_options presence: true do
+      new_record.validate :nickname 
+      new_record.validate :birthday 
 
-    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
-      validates :first_name_kana
-      validates :last_name_kana
+      with_options format: { with: /\A[ぁ-んァ-ン一-龥々]/ } do
+        new_record.validate :first_name
+        new_record.validate :last_name
+      end
+
+      with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+        new_record.validate :first_name_kana
+        new_record.validate :last_name_kana
+      end
     end
+    new_record.validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
   end
-
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_01_003516) do
+ActiveRecord::Schema.define(version: 2021_01_01_090135) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -61,6 +61,16 @@ ActiveRecord::Schema.define(version: 2021_01_01_003516) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.bigint "room_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "item_id"
@@ -68,6 +78,15 @@ ActiveRecord::Schema.define(version: 2021_01_01_003516) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["item_id"], name: "index_orders_on_item_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "room_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_room_users_on_room_id"
+    t.index ["user_id"], name: "index_room_users_on_user_id"
   end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -97,6 +116,10 @@ ActiveRecord::Schema.define(version: 2021_01_01_003516) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "orders"
   add_foreign_key "items", "users"
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
+  add_foreign_key "room_users", "rooms"
+  add_foreign_key "room_users", "users"
 end
